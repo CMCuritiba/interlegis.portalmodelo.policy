d942d9fd7c3dc5c2cf03c4f3b61391f04394bc25
Rafahela <rafahela@gmail.com>
2018-02-19 18:33:25 -0300

    Back to development: 1.0rc8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0rc7'
+version = '1.0rc8.dev0'
 description = u'Pacote de politicas do Portal Modelo.'
 long_description = (
     open('README.rst').read() + '\n' +
